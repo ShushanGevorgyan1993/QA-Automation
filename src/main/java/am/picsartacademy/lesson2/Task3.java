d9ef839a5a2c78dcce8ffbdbcad985fc427b5126
ShushanGevorgyan1993 <gevorgyanshushan.93@gmail.com>
2025-05-12 00:29:12 +0400

    task 7 is done

--- src/main/java/am/picsartacademy/lesson2/Task3.java
+++ src/main/java/am/picsartacademy/lesson2/Task3.java
@@ -2,6 +2,7 @@ package am.picsartacademy.lesson2;
 
 public class Task3 {
     public static void main(String[] args) {
+
         int[] oddNumbers = new int[20];
         int firstNumber = -19;
         for (int i = 0; i < oddNumbers.length; i++) {
--- src/main/java/am/picsartacademy/lesson2/Task6.java
+++ src/main/java/am/picsartacademy/lesson2/Task6.java
@@ -1,7 +1,6 @@
 package am.picsartacademy.lesson2;
 
 public class Task6 {
-
     public static void main(String[] args) {
 
         int[] array = new int[]{2, 8, 34, 5, -5, -12, 23, 46, 26, 198};
